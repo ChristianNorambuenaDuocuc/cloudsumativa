1dd3a6cf4a54f21c4fe11acf567a3c22bc17243e
ChristianNorambuenaDuocuc <chri.norambuena@duocuc.cl>
2026-06-05 14:23:50 -0400

    Update AwsConfig.java

--- transporte/src/main/java/com/sumativa/transporte/config/AwsConfig.java
+++ transporte/src/main/java/com/sumativa/transporte/config/AwsConfig.java
@@ -1,5 +1,18 @@
 package com.sumativa.transporte.config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
 public class AwsConfig {
 
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.US_EAST_1)
+                .build();
+    }
 }
