a35c7762260fb4934a5e4b6bd6eb0469ed2a4506
ChristianNorambuenaDuocuc <chri.norambuena@duocuc.cl>
2026-06-05 14:02:57 -0400

    Update GuiaDespachoService.java

--- transporte/src/main/java/com/sumativa/transporte/service/GuiaDespachoService.java
+++ transporte/src/main/java/com/sumativa/transporte/service/GuiaDespachoService.java
@@ -2,6 +2,7 @@ package com.sumativa.transporte.service;
 
 import com.sumativa.transporte.dto.GuiaDespachoRequestDTO;
 import com.sumativa.transporte.dto.GuiaDespachoResponseDTO;
+import com.sumativa.transporte.model.GuiaDespacho;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -19,4 +20,13 @@ public interface GuiaDespachoService {
     void eliminarGuia(Long id);
 
     List<GuiaDespachoResponseDTO> buscarPorTransportistaYFecha(String transportista, LocalDate fecha);
+
+private String generarRutaS3(GuiaDespacho guia) {
+    String anio = String.valueOf(guia.getFecha().getYear());
+    String transportista = guia.getTransportista().replaceAll("\\s+", "_").toLowerCase();
+
+    return anio + "/" + transportista + "/guia-" + guia.getNumeroGuia() + ".pdf";
+}
+
+
 }
