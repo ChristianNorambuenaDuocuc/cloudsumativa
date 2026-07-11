e4d4b78af3f67d7a6840dd319710d056d7da06e3
ChristianNorambuenaDuocuc <chri.norambuena@duocuc.cl>
2026-07-11 12:31:09 -0400

    Update ResumenConsumidorService.java

--- transporte/src/main/java/com/sumativa/transporte/service/ResumenConsumidorService.java
+++ transporte/src/main/java/com/sumativa/transporte/service/ResumenConsumidorService.java
@@ -12,25 +12,49 @@ public class ResumenConsumidorService {
 
     private final ResumenCompraRepository resumenCompraRepository;
 
-    public ResumenConsumidorService(ResumenCompraRepository resumenCompraRepository) {
+    public ResumenConsumidorService(
+            ResumenCompraRepository resumenCompraRepository
+    ) {
         this.resumenCompraRepository = resumenCompraRepository;
     }
 
     @RabbitListener(queues = RabbitMQConfig.QUEUE_RESUMEN)
     public void consumirResumen(ResumenInscripcionDTO resumenDTO) {
 
+        // Error controlado únicamente para probar
+        // los reintentos y la cola de errores.
+        if ("ERROR-PRUEBA".equals(resumenDTO.getNumeroGuia())) {
+
+            System.out.println(
+                    "Error de prueba para guía: "
+                            + resumenDTO.getNumeroGuia()
+            );
+
+            throw new RuntimeException(
+                    "Error intencional para probar la cola de errores"
+            );
+        }
+
         ResumenCompra resumenCompra = new ResumenCompra();
+
         resumenCompra.setGuiaId(resumenDTO.getGuiaId());
         resumenCompra.setNumeroGuia(resumenDTO.getNumeroGuia());
         resumenCompra.setTransportista(resumenDTO.getTransportista());
         resumenCompra.setFecha(resumenDTO.getFecha());
         resumenCompra.setDestinatario(resumenDTO.getDestinatario());
-        resumenCompra.setDireccionDestino(resumenDTO.getDireccionDestino());
+        resumenCompra.setDireccionDestino(
+                resumenDTO.getDireccionDestino()
+        );
         resumenCompra.setEstado(resumenDTO.getEstado());
-        resumenCompra.setFechaResumen(resumenDTO.getFechaResumen());
+        resumenCompra.setFechaResumen(
+                resumenDTO.getFechaResumen()
+        );
 
         resumenCompraRepository.save(resumenCompra);
 
-        System.out.println("Resumen guardado en Oracle para guía ID: " + resumenDTO.getGuiaId());
+        System.out.println(
+                "Resumen guardado en Oracle para guía ID: "
+                        + resumenDTO.getGuiaId()
+        );
     }
 }
